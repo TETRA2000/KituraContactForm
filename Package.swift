d6b672706c062ff0a5326a36dee2bc23f96f15ad
Takahiko Inayama <sagittariusm25@gmail.com>
2018-03-02 13:37:49 +0900

    add template engins

--- Package.swift
+++ Package.swift
@@ -13,12 +13,13 @@ let package = Package(
       .package(url: "https://github.com/IBM-Swift/SwiftKueryMySQL.git", .upToNextMinor(from: "1.0.0")),
       .package(url: "https://github.com/IBM-Swift/Swift-Kuery-ORM.git", .upToNextMinor(from: "0.0.1")),
       .package(url: "https://github.com/IBM-Swift/Swift-Kuery.git", .branch("master")),
+      .package(url: "https://github.com/IBM-Swift/Kitura-MustacheTemplateEngine.git", .upToNextMajor(from: "1.7.3")),
+      .package(url: "https://github.com/IBM-Swift/Kitura-StencilTemplateEngine.git", .upToNextMajor(from: "1.8.5")),
       // 
     ],
     targets: [
       .target(name: "KituraContactForm", dependencies: [ .target(name: "Application"), "Kitura" , "HeliumLogger"]),
-      .target(name: "Application", dependencies: [ "Kitura", "Configuration", "CloudEnvironment","SwiftMetrics","Health", "SwiftKueryORM", "SwiftKueryMySQL"]),
-
+      .target(name: "Application", dependencies: ["Kitura", "Configuration", "CloudEnvironment","SwiftMetrics","Health", "SwiftKueryORM", "SwiftKueryMySQL", "KituraStencil", "KituraMustache"]),
       .testTarget(name: "ApplicationTests" , dependencies: [.target(name: "Application"), "Kitura","HeliumLogger" ])
     ]
 )
